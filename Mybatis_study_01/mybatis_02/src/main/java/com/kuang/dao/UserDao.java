4746dafcee8ce063007d9178d62d64aa8f8deeb2
Yida Chen <yic192@pitt.edu>
2022-10-04 21:33:28 -0400

    crud

--- Mybatis_study_01/mybatis_02/src/main/java/com/kuang/dao/UserDao.java
+++ Mybatis_study_01/mybatis_02/src/main/java/com/kuang/dao/UserDao.java
@@ -5,5 +5,9 @@ import com.kuang.pojo.User;
 import java.util.List;
 
 public interface UserDao { // dao == mapper
-    List<User> getUserList();
+    List<User> getUserList();// 获取全部用户
+    User getSingleUser(int id); // 获取单个用户
+    void addUser(User user); // 插入单个用户
+    void updateUser(User user); // 修改
+    void deleteUser(int id); // 删除,通过id删除
 }
--- Mybatis_study_01/mybatis_02/src/test/java/com/kuang/dao/UserDaoTest.java
+++ Mybatis_study_01/mybatis_02/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.kuang.dao;
 
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
@@ -30,6 +31,41 @@ public class UserDaoTest {
         }
 
         // 关闭sqlSession
+        sqlSession.close();
+    }
+    @Test
+    public void getUserById(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class); // 获取对象接口
+        User user = userDao.getSingleUser(1);
+        System.out.println(user.getName());
+        sqlSession.close();
+    }
+    // 增删改查需要提交事务
+    @Test
+    public void addUser1(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user1 = new User(1,"jjm","18011784568");
+        userDao.addUser(user1);
+        // 提交事务
+        sqlSession.commit(); // 重点，提交事务
+        sqlSession.close();
+    }
+    @Test
+    public void update(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        userDao.updateUser(new User(5,"cq","13902227308"));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void delete(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        userDao.deleteUser(6);
+        sqlSession.commit();
         sqlSession.close();
     }
 }
