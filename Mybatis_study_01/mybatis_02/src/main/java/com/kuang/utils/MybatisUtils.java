167341634d40c773df5b6a5284f7c747bc0c6711
Yida Chen <yic192@pitt.edu>
2022-10-04 20:59:30 -0400

    add

---
+++ Mybatis_study_01/mybatis_02/src/main/java/com/kuang/utils/MybatisUtils.java
@@ -0,0 +1,29 @@
+package com.kuang.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+// sqlSessionFactory ---> sqlSession
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try{
+            // 获取 sqlSessionFactory 对象 ---- 第一步
+            String resource = "mybatis_config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    //既然有了 SqlSessionFactory，顾名思义，我们可以从中获得 SqlSession 的实例。
+    // SqlSession 提供了在数据库执行 SQL 命令所需的所有方法。你可以通过 SqlSession 实例来直接执行已映射的 SQL 语句。
+    public static SqlSession getSqlSession(){ // 创建 sqlsession 对象
+        return sqlSessionFactory.openSession(); // 数据库对象（可操作
+    }
+}
---
+++ Mybatis_study_01/mybatis_02/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,35 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class UserDaoTest {
+    @Test
+    public void test(){
+        // 获取sqlsession对象，读取对象中的数据库
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        try {
+            // 方式一：getMapper
+            // 执行sql
+            UserDao mapper = sqlSession.getMapper(UserDao.class); // 通过userdao的class对象
+            List<User> userList= mapper.getUserList(); // 方法，获取对象
+
+
+            for(User user : userList){
+                System.out.println(user.getName() + "\t" + user.getPwd());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+
+        // 关闭sqlSession
+        sqlSession.close();
+    }
+}
